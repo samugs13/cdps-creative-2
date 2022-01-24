8decc64be13a8c85eda178d3292ffc25028c3f37
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2022-01-24 01:43:27 +0100

    feature: pass variables in command line

--- gcloud/gcloud.py
+++ gcloud/gcloud.py
@@ -29,11 +29,14 @@ DEPLOY_COMMANDS = [
 def init():
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_APPLICATION_CREDENTIALS
 
-def get_zones():
+def get_zones(regex: str = None):
     try:
-        cmd = "gcloud compute zones list"
+        cmd = "gcloud compute zones list | awk '{print $1}'"
+        if regex:
+            cmd += f" | grep -E '{regex}'"
+
         proc = sp.run(
-            cmd.split(" "), stdout=sp.PIPE, stderr=sp.STDOUT
+            cmd, shell=True, stdout=sp.PIPE, stderr=sp.STDOUT
         )
         proc.check_returncode()
         zones = proc.stdout.decode("utf-8")
@@ -42,9 +45,14 @@ def get_zones():
         exit(0)
 
 
-def create():
+def create(
+    zone: str = INSTANCE_ZONE,
+    name: str = INSTANCE_NAME,
+    id: str = PROJECT_ID
+):
+    print(f"Creating instance {name} in zone {zone} in project {id}")
     try:
-        cmd = f"gcloud compute instances create {INSTANCE_NAME} --project={PROJECT_ID} --zone={INSTANCE_ZONE} --machine-type=e2-medium --network-interface=network-tier=PREMIUM,subnet=default --maintenance-policy=MIGRATE --service-account={SERVICE_ACCOUNT_NAME}@{PROJECT_ID}.iam.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server,https-server --create-disk=auto-delete=yes,boot=yes,device-name={INSTANCE_NAME},image=projects/debian-cloud/global/images/debian-10-buster-v20211209,mode=rw,size=10,type=projects/{PROJECT_ID}/zones/{INSTANCE_ZONE}/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any"
+        cmd = f"gcloud compute instances create {name} --project={id} --zone={zone} --machine-type=e2-medium --network-interface=network-tier=PREMIUM,subnet=default --maintenance-policy=MIGRATE --service-account={SERVICE_ACCOUNT_NAME}@{id}.iam.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server,https-server --create-disk=auto-delete=yes,boot=yes,device-name={name},image=projects/debian-cloud/global/images/debian-10-buster-v20211209,mode=rw,size=10,type=projects/{id}/zones/{zone}/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any"
         proc = sp.run(
             cmd.split(" "), stdout=sp.PIPE, stderr=sp.DEVNULL
         )
@@ -64,11 +72,16 @@ def list_instances(project_id: str, zones: list):
 
     return instances
 
-def deploy():
+def deploy(
+    zone: str = INSTANCE_ZONE,
+    name: str = INSTANCE_NAME,
+    id: str = PROJECT_ID
+):
+    print("Deploying application to instance {name} in zone {zone} of project {id}")
     for sh_cmd in DEPLOY_COMMANDS:
         proc = None
         try:
-            cmd = f"gcloud compute ssh {INSTANCE_NAME} --zone={INSTANCE_ZONE} --project={PROJECT_ID} --command='{sh_cmd}'"
+            cmd = f"gcloud compute ssh {name} --zone={zone} --project={id} --command='{sh_cmd}'"
             print(f"Executing '{sh_cmd}'")
             proc = sp.run(
                 cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE
@@ -83,16 +96,56 @@ def deploy():
 
 def main():
     init()
-    if (len(sys.argv) > 1):
-        if (sys.argv[1] == "create"):
-            create()
-        elif (sys.argv[1] == "list"):
-            if (len(sys.argv) > 2):
-                list_instances(PROJECT_ID, sys.argv[2].split(","))
-            else:
-                list_instances(PROJECT_ID, [INSTANCE_ZONE])
+    if len(sys.argv) > 1:
+        # Get arguments
+        if "--zone" in sys.argv:
+            zone = sys.argv[sys.argv.index("--zone") + 1]
+            sys.argv.remove("--zone")
+            sys.argv.remove(zone)
+        else:
+            zone = INSTANCE_ZONE
+
+        if "--name" in sys.argv:
+            name = sys.argv[sys.argv.index("--name") + 1]
+            sys.argv.remove("--name")
+            sys.argv.remove(name)
+        else:
+            name = INSTANCE_NAME
+
+        if "--id" in sys.argv:
+            id = sys.argv[sys.argv.index("--id") + 1]
+            sys.argv.remove("--id")
+            sys.argv.remove(id)
+        else:
+            id = PROJECT_ID
+
+        if "--zones" in sys.argv:
+            zones_arg = sys.argv[sys.argv.index("--zones") + 1]
+            zones = zones_arg.split(",")
+            sys.argv.remove("--zones")
+            sys.argv.remove(zones_arg)
+        elif "--match" in sys.argv:
+            regex = sys.argv[sys.argv.index("--match") + 1]
+            zones = get_zones(regex)
+            sys.argv.remove("--match")
+            sys.argv.remove(regex)
+        else:
+            zones = None
+            regex = None
+
+        # Execute commands
+        if sys.argv[1] == "create":
+            create(zone, name, id)
+        elif sys.argv[1] == "lszones":
+            print("Available zones:")
+            if not zones: zones = get_zones()
+            for zone in zones:
+                print(f" - {zone}")
+        elif sys.argv[1] == "list":
+            if not zones: zones = get_zones()
+            list_instances(id, zones)
         elif (sys.argv[1] == "deploy"):
-            deploy()
+            deploy(zone, name, id)
         else:
             print("Invalid command")
     else:
